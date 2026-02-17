ddac8ab0c04dc3673ede56be06dad833a6f0e0f3
Rinit7 <swanitrivankar@gmail.com>
2026-02-17 12:00:46 +0530

    made changes to main

--- main.tf
+++ main.tf
@@ -1,14 +1,36 @@
+############################################
+# Terraform Backend Configuration
+############################################
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bamboo-7423"
+    key            = "s3-deployment/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+############################################
+# Provider
+############################################
 provider "aws" {
   region = "ap-south-1"
 }
 
+############################################
 # First Bucket
+############################################
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "bamboo-bucket-7423"
-
-  lifecycle {
-    prevent_destroy = false
-  }
+  bucket        = "bamboo-bucket-7423"
+  force_destroy = true
 
   tags = {
     Name        = "Bamboo Terraform Demo"
@@ -16,7 +38,7 @@ resource "aws_s3_bucket" "demo_bucket" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "versioning" {
+resource "aws_s3_bucket_versioning" "versioning_1" {
   bucket = aws_s3_bucket.demo_bucket.id
 
   versioning_configuration {
@@ -24,13 +46,12 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
+############################################
 # Second Bucket
+############################################
 resource "aws_s3_bucket" "demo_bucket_2" {
-  bucket = "bamboo-bucket-7423-logs"
-
-  lifecycle {
-    prevent_destroy = false
-  }
+  bucket        = "bamboo-bucket-7423-logs"
+  force_destroy = true
 
   tags = {
     Name        = "Bamboo Terraform Demo Logs"
@@ -46,10 +67,11 @@ resource "aws_s3_bucket_versioning" "versioning_2" {
   }
 }
 
+############################################
 # Identity Check
+############################################
 data "aws_caller_identity" "current" {}
 
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
-
